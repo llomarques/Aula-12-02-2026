fd34f7de66e7d311c275771f560894cd3b0d5d83
llomarques <dev@DSN1191348087.edu119.local>
2026-04-14 09:12:05 -0300

    CRUD finalizado

---
+++ Downloads/cliente/src/main/java/br/com/senai/cliente/controller/ClienteController.java
@@ -0,0 +1,62 @@
+package br.com.senai.cliente.controller;
+
+
+import br.com.senai.cliente.cliente.*;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import org.springframework.data.domain.Pageable;
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository repository;
+
+    @GetMapping
+    public Page<DadosListagemCliente> listarCliente (@PageableDefault(size=10, sort = {"nome"})Pageable paginacao){
+        return repository.findAllByAtivoTrue(paginacao)
+                .map(DadosListagemCliente::new);
+    }
+
+    @GetMapping("/{id}")
+    public DadosDetalhamentoCliente detalharCategoria(@PathVariable Long id){
+        Cliente cliente = repository.findByIdAndAtivoTrue(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Cliente não existe"
+                ));
+        return new DadosDetalhamentoCliente(cliente);
+    }
+
+    @PostMapping
+    @Transactional
+    public void cadastrarCliente(@RequestBody @Valid DadosCadastroCliente dados){
+        repository.save(new Cliente(dados));
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizarCliente(@RequestBody @Valid DadosAtualizarCliente dados){
+        var cliente = repository.getReferenceById(dados.id());
+        cliente.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void deletarCliente(@PathVariable Long id){
+        var cliente = repository.getReferenceById(id);
+        cliente.excluirCliente();
+    }
+
+
+}
+    
